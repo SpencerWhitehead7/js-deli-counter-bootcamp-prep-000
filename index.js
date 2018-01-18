a4f24a053e7c336d04c4dc90e44755114078401b
SpencerWhitehead7@gmail.com <spencerwhitehead7@gmail.com>
2018-01-18 21:07:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine,Name){
   katzDeliLine.push(name);
-  return `welcome, #{katzDeliLine[length.katzDeliLine]}. You are number ${length.katzDeliLine} in line.`;
+  return `welcome, ${katzDeliLine[length.katzDeliLine]}. You are number ${length.katzDeliLine} in line.`;
 }
