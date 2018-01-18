d72bd219d38c82217b65170fc0a91fa1bf6b5228
SpencerWhitehead7@gmail.com <spencerwhitehead7@gmail.com>
2018-01-18 21:54:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ function nowServing(katzDeli){
     return `Currently serving ${katzDeli.shift()}.`;
   }
 }
+
+function currentLine(katzDeli){
+  if (katzDeli.length === 0){
+    return "The line is currently empty."
+  }
+  else {
+    var output = `The line is currently:
+    return output
+  }
+}
